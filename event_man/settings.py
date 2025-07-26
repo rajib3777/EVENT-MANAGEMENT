3270db3fdb1680fed8c2ca00c22776033ff5e739
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-07-26 17:14:50 +0600

    dbsql

--- event_man/settings.py
+++ event_man/settings.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'event_man.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # DATABASES = {
@@ -106,15 +106,15 @@ WSGI_APPLICATION = 'event_man.wsgi.application'
 #     )
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'event_db',
-        'USER': 'postgres',
-        'PASSWORD': '11001',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'event_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '11001',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }}
 
 
 # Password validation
