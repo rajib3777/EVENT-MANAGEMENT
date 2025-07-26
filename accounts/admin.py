3d015c2dd0c74c006e5d0326f12bf28c5e6a77a6
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-07-26 17:52:52 +0600

    final

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import Customuser
 # Register your models here.
+class Customuseradmin(UserAdmin):
+    model = Customuser
+    list_display =['username', 'email', 'first_name' , 'last_name', 'role', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role', 'mobile_number', 'profile_picture')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role', 'mobile_number', 'profile_picture')}),
+    )
+    
+admin.site.register(Customuser,Customuseradmin)
