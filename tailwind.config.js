ecaab8a56b3ee80d8e7e4cf6a12eb70343c5239e
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-12-13 13:47:51 +0600

    tailwind correction

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./**/templates/**/*.html",
+    "./**/*.py",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("flowbite/plugin"),
+  ],
+}
+
